0ad3b27088ceadb9300db6644e1fa3d59ab29ec6
ahmedelmelegy <ahmedelmelegy3570@gmail.com>
2023-06-23 16:42:55 +0200

    Add description

--- backup.sh
+++ backup.sh
@@ -4,9 +4,9 @@
 # Date Created: 23/6/2023
 # Last Modified: 23/6/2023
 # Description
-# 
+# Archive contents of home
 
-# Usage:
+# Usage: 
 # 
 
 tar -cvf ~/bash_course/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 1> comp_log.txt 2> error_log.txt
