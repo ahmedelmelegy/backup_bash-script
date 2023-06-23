76f51fc96f164112b9e7747904227333ebe22f4c
gAhmed-Elmelegy <mohamahmed3570@gmail.com>
2023-06-23 16:39:49 +0200

    Archive contents of Home & Redirect messages

--- backup.sh
+++ backup.sh
@@ -3,12 +3,11 @@
 # Author: Ahmed Elmelegy
 # Date Created: 23/6/2023
 # Last Modified: 23/6/2023
-
 # Description
 # 
 
 # Usage:
 # 
 
-echo "Test script"
+tar -cvf ~/bash_course/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 1> comp_log.txt 2> error_log.txt
 exit 0
