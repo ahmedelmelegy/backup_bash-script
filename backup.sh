6e5138a04d53f22027ca2f8e4ec1f50fc5c7d4de
ahmedelmelegy <ahmedelmelegy3570@gmail.com>
2023-06-25 22:07:04 +0200

    Add positional parameters for backup.sh and restore.sh

--- backup.sh
+++ backup.sh
@@ -2,17 +2,17 @@
 
 # Author: Ahmed Elmelegy
 # Date Created: 23/6/2023
-# Last Modified: 24/6/2023
+# Last Modified: 25/6/2023
 
 # Description
 # Creates a backup in ~/bash_course of all files in the home directory
 
 # Usage: 
 # backup.sh
-
-source=${HOME}
-dest=./my_backup
 echo "Hello, ${USER^}"
-tar -cvf ${dest}_"$(date +%d-%m-%Y_%H-%M-%S)".tar ${source}/* 1> comp_log.txt 2> error_log.txt
+source=$1
+dest=$2
+
+tar -cvf ${dest}my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ${source}/* 1> comp_log.txt 2> error_log.txt
 echo "Backup Completed Successfully."
 exit 0
--- restore.sh
+++ restore.sh
@@ -2,7 +2,7 @@
 
 # Author: Ahmed Elmelegy
 # Date Created: 24/6/2023
-# Last Modified: 24/6/2023
+# Last Modified: 25/6/2023
 
 # Description
 # extract contents of .tar in destination directory user specify
@@ -10,11 +10,11 @@
 # Usage: 
 # restore.sh
 
-source=./my_backup_*
-dest=./uncomp/
-
+source=$1
+dest=$2
+mkdir ${dest}uncomp
 echo "Hello, ${USER^}"
-tar -xvf ${source} -C ${dest} 1> uncomp_log.txt 2> uncomp_error_log.txt
+tar -xvf ${source}my_backup_* -C ${dest}/uncomp 1> uncomp_log.txt 2> uncomp_error_log.txt
 echo "Extraction Completed Successfully."
 
 exit 0
