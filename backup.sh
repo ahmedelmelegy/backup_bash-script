4fdb9740825504b702778562f9ba5f7b9971b860
ahmedelmelegy <ahmedelmelegy3570@gmail.com>
2023-06-25 22:20:21 +0200

    Fix path

--- backup.sh
+++ backup.sh
@@ -8,11 +8,14 @@
 # Creates a backup in destination_path of all files in the source_path
 
 # Usage: 
-# backup.sh source_path destination_path
+# ./backup.sh source_path destination_path
+# For example
+# ./backup.sh ~ .
+
 echo "Hello, ${USER^}"
 source=$1
 dest=$2
 
-tar -cvf ${dest}my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ${source}/* 1> comp_log.txt 2> error_log.txt
+tar -cvf ${dest}/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ${source}/* 1> comp_log.txt 2> error_log.txt
 echo "Backup Completed Successfully."
 exit 0
--- restore.sh
+++ restore.sh
@@ -5,16 +5,18 @@
 # Last Modified: 25/6/2023
 
 # Description
-# extract contents of .tar in source_path to destination_path that user specify
+# extract contents of .tar in source_path to destination_path/uncomp
 
 # Usage: 
 # ./restore.sh source_path destination_path
+# For example
+# ./restore.sh . .
 
 source=$1
 dest=$2
-mkdir ${dest}uncomp
+mkdir ${dest}/uncomp
 echo "Hello, ${USER^}"
-tar -xvf ${source}my_backup_* -C ${dest}/uncomp 1> uncomp_log.txt 2> uncomp_error_log.txt
+tar -xvf ${source}/my_backup_* -C ${dest}/uncomp 1> uncomp_log.txt 2> uncomp_error_log.txt
 echo "Extraction Completed Successfully."
 
 exit 0
