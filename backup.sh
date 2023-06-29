594f6f07c3cd39841831f8df060b6558573c0417
ahmedelmelegy <ahmedelmelegy3570@gmail.com>
2023-06-29 11:36:19 +0200

    Add fourth parameter to backup only the changed files during the last n days

--- backup.sh
+++ backup.sh
@@ -24,13 +24,22 @@ fi
 source=$1
 dest=${2}
 encryption_key=$3
+days=$4
 
 if [[ -z $encryption_key ]]; then
   echo "No encryption key provided. Backup will not be encrypted."
-  tar -cvf "${dest}/my_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar "${source}"/* 1> comp_log.txt 2> error_log.txt
+  if [[ -z $days ]]; then
+    tar -cvf "${dest}/my_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar "${source}"/* 1> comp_log.txt 2> error_log.txt
+  else
+    find "${source}" -type f -mtime -$days -exec tar -rvf "${dest}/my_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar {} \; 1> comp_log.txt 2> error_log.txt
+  fi
 else
   echo "Encrypting backup with the provided encryption key."
-  tar -cvf - "${source}"/* | openssl enc -aes-256-cbc -salt -k "${encryption_key}" -out "${dest}/my_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar 1> comp_log.txt 2> error_log.txt
+  if [[ -z $days ]]; then
+    tar -cvf - "${source}"/* | openssl enc -aes-256-cbc -salt -k "${encryption_key}" -out "${dest}/my_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar 1> comp_log.txt 2> error_log.txt
+  else
+    find "${source}" -type f -mtime -$days -exec tar -cvf - {} \; | openssl enc -aes-256-cbc -salt -k "${encryption_key}" -out "${dest}/my_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar 1> comp_log.txt 2> error_log.txt
+  fi
 fi
 
 echo "Backup Completed Successfully."
