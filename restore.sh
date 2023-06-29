87f9cbcdb92f9c5bb828be83b588f4520e0d4bba
ahmedelmelegy <ahmedelmelegy3570@gmail.com>
2023-06-29 12:02:54 +0200

    Modify Description and Usage

--- restore.sh
+++ restore.sh
@@ -2,31 +2,33 @@
 
 # Author: Ahmed Elmelegy
 # Date Created: 24/6/2023
-# Last Modified: 25/6/2023
+# Last Modified: 29/6/2023
 
 # Description
-# extract contents of .tar in source_path to destination_path/uncomp
-# If user didn`t specify dest_path it will be working directory`
+# Restores the backup by decrypting and extracting the files in reverse order.
 
-# Usage: 
-# ./restore.sh source_path destination_path
-# For example
-# ./restore.sh . .
+# Usage:
+# ./restore.sh backup_directory destination_directory decryption_key
+# For example:
+# ./restore.sh /path/to/backup /path/to/destination mydecryptionkey
 
 echo "Hello, ${USER^}"
 
-if [[ $# -ne 2 ]]; then
-  echo "You didn't enter exactly 2 parameters"
-  echo "Usage: $0 source_path destination_path"
+if [[ $# -ne 3 ]]; then
+  echo "You didn't enter exactly 3 parameters"
+  echo "Usage: $0 source_path destination_path decryption_key"
   exit 1
 fi
 
 source=$1
 dest=${2}
+decryption_key=$3
 
-mkdir ${dest}/uncomp
+mkdir -p ${dest}/uncomp
 echo "Hello, ${USER^}"
-tar -xvf "${source}/my_backup_*" -C "${dest}/uncomp" 1> uncomp_log.txt 2> uncomp_error_log.txt
+gpg --batch --yes --passphrase "$decryption_key" -o "${dest}/uncomp/my_backup.tar" -d "${source}/my_backup_*"
+
+tar -xvf "${dest}/uncomp/my_backup.tar" -C "${dest}/uncomp" 1> uncomp_log.txt 2> uncomp_error_log.txt
 echo "Extraction Completed Successfully."
 
 exit 0
