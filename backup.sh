40f410800fdce80171d1aa28e126037dfca829cf
ahmedelmelegy <ahmedelmelegy3570@gmail.com>
2023-06-29 11:48:52 +0200

    Validate directories

--- backup.sh
+++ backup.sh
@@ -15,10 +15,9 @@
 
 echo "Hello, ${USER^}"
 
-# Check if the script received 4 parameters
 if [[ $# -ne 4 ]]; then
-  echo "Error: Invalid number of parameters."
-  echo "Usage: $0 target_directory destination_directory encryption_key n"
+  echo "You didn't enter the correct number of parameters"
+  echo "Usage: $0 source_path destination_path [encryption_key]"
   exit 1
 fi
 
@@ -31,13 +30,13 @@ if [[ $1 == "-h" || $1 == "--help" ]]; then
   exit 0
 fi
 
-target_directory=$1
-destination_directory=$2
+source=$1
+dest=${2}
 encryption_key=$3
-n=$4
+days=$4
 
 # Validate directories
-if [[ ! -d $target_directory ]]; then
+if [[ ! -d $target ]]; then
   echo "Error: Invalid target directory."
   exit 1
 fi
@@ -47,42 +46,33 @@ if [[ ! -d $destination_directory ]]; then
   exit 1
 fi
 
-# Take a snapshot of the full date
-snapshot_date=$(date +%d-%m-%Y_%H-%M-%S)
-snapshot_date=${snapshot_date// /_}
-snapshot_date=${snapshot_date//:/_}
-
-# Create a directory with the snapshot date under the destination directory
-backup_directory="${destination_directory}/${snapshot_date}"
-mkdir -p "$backup_directory"
-
-# Loop over directories under the target directory
-for directory in "${target_directory}"/*/; do
-  directory_name=$(basename "$directory")
-
-  # Check modification date and backup only modified files within the last n days
-  if [[ -z $n ]]; then
-    find "$directory" -type f -exec tar -cvf "${backup_directory}/${directory_name}_${snapshot_date}.tar" {} \;
+if [[ -z $encryption_key ]]; then
+  echo "No encryption key provided. Backup will not be encrypted."
+  if [[ -z $days ]]; then
+    for dir in "$target"/*/; do
+      dir_name=$(basename "$dir")
+      tar -cvf "${dest}/${dir_name}_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar.gz -C "$dir" . 1> comp_log.txt 2> error_log.txt
+    done
   else
-    find "$directory" -type f -mtime -$n -exec tar -cvf "${backup_directory}/${directory_name}_${snapshot_date}.tar" {} \;
+    for dir in "$target"/*/; do
+      dir_name=$(basename "$dir")
+      find "$dir" -type f -mtime -$days -exec tar -rvf "${dest}/${dir_name}_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar.gz {} \; 1> comp_log.txt 2> error_log.txt
+    done
   fi
-
-  # Encrypt the backup using the provided encryption key
-  gpg --batch --yes --passphrase "$encryption_key" -c "${backup_directory}/${directory_name}_${snapshot_date}.tar"
-
-  # Delete the original tar file and keep the encrypted one
-  rm "${backup_directory}/${directory_name}_${snapshot_date}.tar"
-done
-
-# Enumerate all files directly under the target directory and create a combined backup
-tar -cvf "${backup_directory}/all_files_${snapshot_date}.tar" -C "$target_directory" .
-gzip "${backup_directory}/all_files_${snapshot_date}.tar"
-
-# Encrypt the combined backup using the provided encryption key
-gpg --batch --yes --passphrase "$encryption_key" -c "${backup_directory}/all_files_${snapshot_date}.tar.gz"
-
-# Delete the original tar.gz file
-rm "${backup_directory}/all_files_${snapshot_date}.tar.gz"
+else
+  echo "Encrypting backups with the provided encryption key."
+  if [[ -z $days ]]; then
+    for dir in "$target"/*/; do
+      dir_name=$(basename "$dir")
+      tar -cvf - -C "$dir" . | openssl enc -aes-256-cbc -salt -k "${encryption_key}" -out "${dest}/${dir_name}_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar.gz 1> comp_log.txt 2> error_log.txt
+    done
+  else
+    for dir in "$target"/*/; do
+      dir_name=$(basename "$dir")
+      find "$dir" -type f -mtime -$days -exec tar -cvf - {} \; | openssl enc -aes-256-cbc -salt -k "${encryption_key}" -out "${dest}/${dir_name}_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar.gz 1> comp_log.txt 2> error_log.txt
+    done
+  fi
+fi
 
 echo "Backup Completed Successfully."
 exit 0
