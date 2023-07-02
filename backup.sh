aa3befd4bcff8c18119637b04ee2dfedfd428b70
ahmedelmelegy <ahmedelmelegy3570@gmail.com>
2023-07-02 17:37:12 +0200

    Check source and dest directory validity

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 # Author: Ahmed Elmelegy
 # Date Created: 23/6/2023
-# Last Modified: 25/6/2023
+# Last Modified: 29/6/2023
 
 # Description
 # Creates a backup in destination_path of all files in the source_path
@@ -15,7 +15,7 @@
 
 echo "Hello, ${USER^}"
 
-if [[ $# -ne 2 ]]; then
+if [ $# -ne 2 ]; then
   echo "You didn't enter exactly 2 parameters"
   echo "Usage: $0 source_path destination_path"
   exit 1
@@ -23,6 +23,16 @@ fi
 
 source=$1
 dest=${2}
+
+if [ ! -d $source ]; then
+  echo "Invalid source path"
+  exit 1
+fi
+
+if [ ! -d $dest ]; then
+  echo "Invalid destination path"
+  exit 1
+fi
 
 tar -cvf "${dest}/my_backup_$(date +%d-%m-%Y_%H-%M-%S)".tar "${source}"/* 1> comp_log.txt 2> error_log.txt
 echo "Backup Completed Successfully."
